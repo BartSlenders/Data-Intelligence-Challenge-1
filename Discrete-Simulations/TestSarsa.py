9f7b9f02b7f6aae30d20c4e6a45736594eabca7c
Mihail Tifrea <t.mihaita@yahoo.com>
2022-05-19 14:31:41 +0200

    new combination of parameters

--- Discrete-Simulations/TestSarsa.py
+++ Discrete-Simulations/TestSarsa.py
@@ -15,14 +15,15 @@ efficiencies = []
 n_moves = []
 cleaned = []
 
-
 """
     Executes a run of Policy iteration. 
     @g: the gamma parameter
     @t: the theta parameter
     @c: the certainty used for policy iteration 
 """
-def run(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, grid_file = 'house.grid'):
+
+
+def run(gamma=0.9, epsilon=0.1, alpha=0.5, episodes=200, steps=200, grid_file='house.grid'):
     deaths = 0
     # Open the grid file.
     # (You can create one yourself using the provided editor).
@@ -37,7 +38,7 @@ def run(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, grid_fil
     while True:
         n_epochs += 1
         # Do a robot epoch (basically call the robot algorithm once):
-        robot_epoch(robot, gamma=gamma, epsilon=epsilon, alpha=alpha, episodes = episodes, steps = steps)
+        robot_epoch(robot, gamma=gamma, epsilon=epsilon, alpha=alpha, episodes=episodes, steps=steps)
         # Stop this simulation instance if robot died :( :
         if not robot.alive:
             deaths += 1
@@ -58,6 +59,7 @@ def run(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, grid_fil
         efficiency = (100 * n_total_tiles) / (n_total_tiles + n_revisted_tiles)
     return clean_percent, efficiency
 
+
 """
     Plots violin plots representing the distribution of cleanliness and efficiency
      of 10 consecutive runs of Policy iteration. 
@@ -65,7 +67,9 @@ def run(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, grid_fil
     @t: the theta parameter
     @c: the certainty used for policy iteration 
 """
-def plotDistribution(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, runs=10):
+
+
+def plotDistribution(gamma=0.9, epsilon=0.1, alpha=0.5, episodes=200, steps=200, runs=10):
     cleaned = []
     efficiencies = []
     for i in range(runs):
@@ -73,18 +77,21 @@ def plotDistribution(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 
         cleaned.append(cleaned_a)
         efficiencies.append(efficiency)
     my_array = np.array([cleaned, efficiencies]).T
-    df = pd.DataFrame(my_array, columns = ['cleaned', 'efficiencies'])
+    df = pd.DataFrame(my_array, columns=['cleaned', 'efficiencies'])
     ax = sns.violinplot(data=df)
     ax.set_ylabel("iteration policy performance (%)")
     ax.set_title("distribution of robot performance", size=18, weight='bold');
     plt.show()
+
 
 """
     Plots violin plots representing the distribution of cleanliness of 10 consecutive runs of Policy iteration on
      multiple grids. 
     @g: the gamma parameter
 """
-def plotcleanness(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200, runs=10):
+
+
+def plotcleanness(gamma=0.9, epsilon=0.1, alpha=0.5, episodes=200, steps=200, runs=10):
     grid_files = ["death.grid", "house.grid", "example-random-house-3.grid", "snake.grid"]
     grid_files_names = ["death", "house", "random-house-3", "snake"]
     array = []
@@ -96,19 +103,22 @@ def plotcleanness(gamma=0.9, epsilon=0.1, alpha=0.5, episodes = 200, steps = 200
         array.append(efficiencies)
 
     my_array = np.array(array).T
-    df = pd.DataFrame(my_array, columns = grid_files_names)
-    ax =sns.violinplot(data=df)
+    df = pd.DataFrame(my_array, columns=grid_files_names)
+    ax = sns.violinplot(data=df)
 
     ax.set_ylabel("efficiency (%)")
     ax.set_title("policy iteration - efficiency vs grid", size=18, weight='bold');
     plt.show()
+
 
 """
     Generates a csv file under the name "results.csv" containing the probabilities and efficiencies of multiple runs
     of policy iteration, together with the parameters used.
     @gamma: the gamma parameter
 """
-def generate_results(gamma, epsilon, alpha, episodes, steps, runs_per_combination= 3):
+
+
+def generate_results(gamma, epsilon, alpha, episodes, steps, runs_per_combination=3):
     rows = []
     for g in gamma:
         for e in epsilon:
@@ -124,12 +134,12 @@ def generate_results(gamma, epsilon, alpha, episodes, steps, runs_per_combinatio
     df = pd.DataFrame(my_array, columns=['gamma', 'epsilon', 'alpha', 'episodes', 'steps', 'cleaned', 'efficiency'])
     df.to_csv("results.csv")
 
-gamma = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.99])
+gamma = np.array([0.5, 0.7, 0.9])
 # Epsilon from epsilon-greedy
-epsilon = np.array([0.001, 0.05, 0.1, 0.3])
+epsilon = np.array([0.05, 0.1, 0.2])
 # Learning rate
-alpha = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.99])
-episodes = np.array([10, 50, 100, 200, 400])
-steps = np.array([100, 200, 400, 800])
+alpha = np.array([0.3, 0.6, 0.9])
+episodes = np.array([50, 150, 300])
+steps = np.array([50, 150, 300])
 
 generate_results(gamma, epsilon, alpha, episodes, steps)
